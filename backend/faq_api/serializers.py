bfa6c14bd828c438692927b04ddd9f1862bf37d4
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-04 16:47:46 +0200

    Create serializers.py

---
+++ backend/faq_api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+class ClusterResultSerializer(serializers.Serializer):
+    cluster_id = serializers.IntegerField()
+    message_count = serializers.IntegerField()
+    top_message = serializers.CharField()
+    matched_faq = serializers.CharField()
+    similarity = serializers.FloatField()
+    gpt_evaluation = serializers.CharField()
