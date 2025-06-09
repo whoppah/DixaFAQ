4ad2fadcf91aa95bfdf8eb8278f3d4a084887511
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-09 16:31:17 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-
+WSGI_APPLICATION = "config.wsgi.application"
 # === MIDDLEWARE ===
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",  
