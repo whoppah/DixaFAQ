b8038b67762598b5803c82465426c2b901376ae3
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-09 20:48:15 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,8 @@ CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_BACKEND = None  
-CELERY_TIMEZONE = "UTC"
+USE_TZ = True
+TIME_ZONE = "Europe/Amsterdam"
 
 # === APPLICATIONS ===
 INSTALLED_APPS = [
