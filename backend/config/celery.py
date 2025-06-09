ec945cdf3597c22289a573eda66f12ab21870152
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-09 20:47:42 +0200

    Update celery.py

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -8,13 +8,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("faq_project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-app.conf.timezone = 'UTC'
+app.conf.timezone = 'Europe/Amsterdam'
 
 # Celery Beat schedule
 app.conf.beat_schedule = {
     "weekly-download-job": {
         "task": "faq_api.tasks.async_download_and_process",
-        "schedule": crontab(minute=0, hour=7, day_of_week=1),  # Every Monday at 7:00 AM
+        "schedule": crontab(minute=50, hour=20, day_of_week=1),  # Every Monday at 8:50 PM
         #"options": {"expires": 3600},  # Optional: expires in 1 hour
     },
 }
