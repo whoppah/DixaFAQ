2452884a0d6642da370abd95100159b6851f4620
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-09 22:52:25 +0200

    Update celery.py

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -14,7 +14,8 @@ app.conf.timezone = 'Europe/Amsterdam'
 # Celery Beat schedule
 app.conf.beat_schedule = {
     "weekly-download-job": {
-        "task": "faq_api.tasks.async_download_and_process",
+        "task": "faq_api.task.async_download_and_process",
+        "task": "faq_api.task.log_timezone_debug",
         "schedule": crontab(minute=50, hour=22, day_of_week=1),  # Every Monday at 10:50 PM
         "options": {"expires": 3600},  #expires in 1 hour
     },
