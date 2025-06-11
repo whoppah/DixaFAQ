61db4e17f14e7f25d508af0729733c561dcf105b
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-11 15:28:09 +0200

    Update createadmin.py

--- backend/management/commands/createadmin.py
+++ backend/management/commands/createadmin.py
@@ -7,9 +7,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        username = "admin"
-        email = "admin@example.com"
-        password = os.environ.get("ADMIN_PASSWORD", "admin123")
+        username = "mattia"
+        email = "mattia@whoppah.com"
+        password = os.environ.get("ADMIN_PASSWORD", "whoppah123")
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username, email, password)
