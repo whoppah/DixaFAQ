a4f672201d602fa0b80ba85a0e5e01304cb0e21c
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-11 14:43:47 +0200

    Update urls.py

--- backend/faq_api/urls.py
+++ backend/faq_api/urls.py
@@ -1,8 +1,10 @@
 #backend/faq_api/urls.py
 from django.urls import path
 from .views import cluster_results
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('clusters/', cluster_results),
     path("api/trigger-pipeline/", views.trigger_pipeline),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
 ]
