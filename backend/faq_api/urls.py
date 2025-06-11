0bfc5b8f4015723536e163de047fdfbbe76cba7a
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-11 11:51:41 +0200

    Update urls.py

--- backend/faq_api/urls.py
+++ backend/faq_api/urls.py
@@ -1,3 +1,4 @@
+from faq_api.views import run_migrations
 #backend/faq_api/urls.py
 from django.urls import path
 from .views import cluster_results
@@ -5,4 +6,5 @@ from .views import cluster_results
 urlpatterns = [
     path('clusters/', cluster_results),
     path("api/trigger-pipeline/", views.trigger_pipeline),
+    path("admin/run-migrations/", run_migrations),  # TEMP endpoint
 ]
