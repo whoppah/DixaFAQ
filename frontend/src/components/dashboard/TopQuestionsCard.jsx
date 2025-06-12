625e8c75abd373a044d4c2a6d32d756d00966d18
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-12 12:38:58 +0200

    Update and rename frontend/src/components/TopQuestionsCard.jsx to frontend/src/components/dashboard/TopQuestionsCard.jsx

--- frontend/src/components/dashboard/TopQuestionsCard.jsx
+++ frontend/src/components/dashboard/TopQuestionsCard.jsx
@@ -1,4 +1,4 @@
-//frontend/src/components/TopQuestionsCard.jsx
+//frontend/src/components/dashboard/TopQuestionsCard.jsx
 import React from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/Card";
 
