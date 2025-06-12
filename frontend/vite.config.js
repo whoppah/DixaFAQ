f3e9ffa4dc5f8630d1786b16ae995f7fd3d4313e
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-12 12:29:02 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/', 
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), 
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8000',  // Django dev server
