fffb2a705e1b0d453d4e273c20657a7133fb6b26
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-13 14:00:30 +0200

    Update celery.py

--- backend/faq_api/celery.py
+++ backend/faq_api/celery.py
@@ -15,7 +15,7 @@ app.conf.timezone = 'Europe/Amsterdam'
 app.conf.beat_schedule = {
     "weekly-download-job": {
         "task": "download_and_process",
-        "schedule": crontab(minute=55, hour=11, day_of_week=5),  # Every Monday at 5.00 AM
+        "schedule": crontab(minute=10, hour=12, day_of_week=5),  # Every Monday at 5.00 AM
         #"options": {"expires": 9000},  #expires in 2.5 hour
     },
 }
