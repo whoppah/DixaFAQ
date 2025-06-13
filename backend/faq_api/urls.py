7393f919ef6edb6b12489cf765344eec127ea2bb
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-13 10:29:31 +0200

    Update urls.py

--- backend/faq_api/urls.py
+++ backend/faq_api/urls.py
@@ -1,10 +1,20 @@
 #backend/faq_api/urls.py
-from django.urls import path
-from .views import cluster_results
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 from django.contrib.auth import views as auth_views
 
+router = DefaultRouter()
+router.register(r"messages", views.MessageViewSet, basename="message")
+router.register(r"faqs", views.FAQViewSet, basename="faq")
+router.register(r"cluster-runs", views.ClusterRunViewSet, basename="clusterrun")
+router.register(r"cluster-results", views.ClusterResultViewSet, basename="clusterresult")
+
 urlpatterns = [
-    path('clusters/', cluster_results),
+    path("", include(router.urls)),
+    path("clusters/", views.cluster_results),
+    path("dashboard-clusters-with-messages/", views.dashboard_clusters_with_messages),
     path("api/trigger-pipeline/", views.trigger_pipeline),
+    path("api/current-user-info/", views.current_user_info),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
 ]
