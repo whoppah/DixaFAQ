c1b8cf7b040060edc7913b1e12096f0abcc4b083
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-27 12:55:06 +0200

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "",
-  withCredentials: true,
+  withCredentials: false,
 });
 
 instance.interceptors.request.use((config) => {
