89e8b46fb0200dd42f230b45d503ce1cdda02366
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-27 12:58:21 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://backend-dixafaq-production.up.railway.app"]  
-CSRF_TRUSTED_ORIGINS = ["https://frontend-dixafaq-production.up.railway.app","https://backend-dixafaq-production.up.railway.app"]  
+ALLOWED_HOSTS = ["https://frontend-dixafaq-production-853e.up.railway.app","https://backend-dixafaq-production.up.railway.app"]  
+CSRF_TRUSTED_ORIGINS = ["https://frontend-dixafaq-production-853e.up.railway.app","https://backend-dixafaq-production.up.railway.app"]  
 
 # === API Keys ===
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
@@ -88,13 +88,13 @@ MIDDLEWARE = [
 # === CORS CONFIG ===
 CORS_ALLOW_CREDENTIALS = True  
 CORS_ALLOWED_ORIGINS = [
-    "https://frontend-dixafaq-production.up.railway.app",
+    "https://frontend-dixafaq-production-853e.up.railway.app",
     "https://backend-dixafaq-production.up.railway.app",
 ]
 
 # === CSRF CONFIG ===
 CSRF_TRUSTED_ORIGINS = [
-    "https://frontend-dixafaq-production.up.railway.app",
+    "https://frontend-dixafaq-production-853e.up.railway.app",
     "https://backend-dixafaq-production.up.railway.app",
 ]
 
