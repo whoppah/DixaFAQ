5cbf0d7527f297ff8b8cd9e2eb381d156aa9c67f
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-27 10:56:12 +0200

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,22 @@
+// src/lib/axios.js
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "",
   withCredentials: true,
 });
 
+instance.interceptors.request.use((config) => {
+  const csrfToken = Cookies.get("csrftoken");
+
+  const safeMethods = ["post", "put", "patch", "delete"];
+  if (csrfToken && safeMethods.includes(config.method?.toLowerCase())) {
+    config.headers["X-CSRFToken"] = csrfToken;
+  }
+
+  return config;
+});
+
 export default instance;
+
