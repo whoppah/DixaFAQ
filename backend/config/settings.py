deb55b219efdef331fec8cf067482c0742a14a63
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-27 13:58:00 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,6 +6,8 @@ from pathlib import Path
 # === BASE DIRECTORY ===
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# === CONFIG URL ===
+ROOT_URLCONF = "config.urls"
 # === SECURITY KEYS ===
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key")
 
