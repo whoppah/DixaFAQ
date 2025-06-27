003d90e69d95f91ff45aaae3f55c12896b001a97
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-27 14:20:14 +0200

    Update views.py

--- backend/faq_api/views.py
+++ backend/faq_api/views.py
@@ -1,6 +1,6 @@
 #backend/faq_api/views.py
 from rest_framework.decorators import api_view, permission_classes, action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import viewsets, filters
 from django.conf import settings
