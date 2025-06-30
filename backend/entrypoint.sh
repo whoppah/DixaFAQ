421bbe0f13c45156379698474550fe72b09bf3b2
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-06-30 09:59:39 +0200

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -15,6 +15,6 @@ fi
 echo "Starting Gunicorn server..."
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:8080 \
-    --workers 2 \
+    --workers 4 \
     --timeout 120 \
     --log-level info
