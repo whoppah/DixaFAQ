18d94c4c9d7a9842a4a480a247bdfd510b7954b9
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-25 10:23:09 +0200

    Update celery.py

--- backend/faq_api/celery.py
+++ backend/faq_api/celery.py
@@ -15,6 +15,6 @@ app.conf.timezone = 'Europe/Amsterdam'
 app.conf.beat_schedule = {
     "weekly-download-job": {
         "task": "faq_api.tasks.start_pipeline",
-        "schedule": crontab(minute=59, hour=9, day_of_week=5),  # Every Monday at 7:00 AM
+        "schedule": crontab(minute=30, hour=10, day_of_week=5),  # Every Monday at 7:00 AM
     },
 }
